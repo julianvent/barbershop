638e6c92ff1b5302eb48b3fdf64aeea6d3d02fba
MrSheldonT <kukuo798@gmail.com>
2025-12-07 19:36:46 -0600

    fix(env): use correct BASE_URL_FRONTEND and BASE_URL_BACKEND environment variables

--- backend/services/jwt.service.js
+++ backend/services/jwt.service.js
@@ -2,18 +2,19 @@ import jwt from 'jsonwebtoken';
 import dotenv from "dotenv";
 dotenv.config();
 
-const ACCESS_TOKEN_SECRET  = process.env.JWT_SECRET || 'a2V2aWlpaWlpbg==';
-const ACCESS_TOKEN_EXPIRES_IN  = process.env.JWT_EXPIRES || '1d';
+const ACCESS_TOKEN_SECRET = process.env.JWT_SECRET || 'a2V2aWlpaWlpbg==';
+const ACCESS_TOKEN_EXPIRES_IN = process.env.JWT_EXPIRES || '1d';
 const SIGNED_URL_SECRET = process.env.SIGNED_URL_SECRET || 'a2V2aWlpaWlpbjI='
 const SIGNED_URL_EXPIRES_IN = process.env.SIGNED_URL_EXPIRES_IN || '90d'
-export const BASE_URL = process.env.BASE_URL || "https://sagozbarberdev.com";
+export const BASE_URL_FRONTEND = process.env.BASE_URL_FRONTEND || "https://sagozbarber.com";
+export const BASE_URL_BACKEND = process.env.BASE_URL_BACKEND || "https://sagozbarberdev.com";
 
 export function signAccessToken(payload, opts = {}) {
   return jwt.sign(
-            payload,
-            ACCESS_TOKEN_SECRET ,
-            { expiresIn: ACCESS_TOKEN_EXPIRES_IN, ...opts }
-          );
+    payload,
+    ACCESS_TOKEN_SECRET,
+    { expiresIn: ACCESS_TOKEN_EXPIRES_IN, ...opts }
+  );
 }
 
 export function verifyToken(token) {
@@ -34,14 +35,14 @@ export function verifyToken(token) {
   }
 }
 
-export function generateAppointmentLink(appointmentId){
+export function generateAppointmentLink(appointmentId) {
   const token = jwt.sign(
     { appointmentId },
     SIGNED_URL_SECRET,
     { expiresIn: SIGNED_URL_EXPIRES_IN }
   );
 
-  return `${BASE_URL}/appointments/${appointmentId}?auth=${token}`;
+  return `${BASE_URL_FRONTEND}/appointments/${appointmentId}?auth=${token}`;
 }
 
 export function verifyTokenAppointment(token) {
